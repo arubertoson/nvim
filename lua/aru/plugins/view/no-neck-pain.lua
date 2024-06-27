3a9ebacb4db8b5d6e13f0181a848f8e85dbc2bfd
Marcus Albertsson <marcus.arubertoson@gmail.com>
2024-06-27 17:36:24 +0200

    fix the no-neck plugin for buffer centration

--- lua/aru/plugins/view/no-neck-pain.lua
+++ lua/aru/plugins/view/no-neck-pain.lua
@@ -1,3 +1,17 @@
+local function close_other_windows()
+	local windows = vim.api.nvim_tabpage_list_wins(0)
+	if #windows == 1 then
+		return
+	end
+
+	local current_win = vim.api.nvim_get_current_win()
+	for _, win in ipairs(windows) do
+		if win ~= current_win then
+			vim.api.nvim_win_close(win, true)
+		end
+	end
+end
+
 local function golden_ratio()
 	-- We check the current width of the existing window and
 	-- overwrite the no-neck-pain config
@@ -7,12 +21,34 @@ end
 return {
 	{
 		"shortcuts/no-neck-pain.nvim",
-		cmd = { "NoNeckPain" },
-		keys = {
-			{
-				"<leader>o",
+		version = "*",
+		lazy = false,
+		opts = {
+			debug = true,
+			minSideBufferWidth = 10,
+			disableOnLastBuffer = false,
+			killAllBuffersOnDisable = false,
+			fallbackOnBufferDelete = true,
+			buffers = {
+				colors = {
+					background = "rose-pine-moon",
+					blend = -0.5,
+				},
+			},
+		},
+		config = function(opts)
+			require("no-neck-pain").setup(opts.opts)
+
+			require("aru.utils.keymaps").set({
+				{ "n" },
+				"<leader>wo",
 				function()
 					local nnp = require("no-neck-pain")
+
+					-- We need to ensure that only one windows before firing the
+					-- toggle to ensure that we don't screw up other windows. It's
+					-- better to start from a clean slate afterwards.
+					close_other_windows()
 
 					-- For some reason if we don't use the normal "command"
 					-- routes we have to set the options our selves.
@@ -24,9 +60,13 @@ return {
 					nnp.config.width = golden_ratio()
 					nnp.toggle()
 				end,
-			},
-		},
-		config = function()
+				{
+					desc = "NoNeckPain Toggle",
+				},
+			})
+
+			-- To keep the window nice and centered in the correct ratio we add a little
+			-- sweet autocmd to operate on the window resize.
 			require("aru.utils").create_augroup("no-neck-pain-resize", {
 				{
 					event = { "VimResized" },
