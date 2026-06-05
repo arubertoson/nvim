0fafd9955870cac7316ccfa44dfc6f34bdf9f848
Marcus Albertsson <2284432+arubertoson@users.noreply.github.com>
2026-06-05 12:44:33 +0200

    feat(oil): add toggle for hiding gitignored files
    Pin plugin versions and add Oil support for toggling gitignored files from the file browser.

--- lua/aru/pack.lua
+++ lua/aru/pack.lua
@@ -42,33 +42,32 @@ vim.pack.add({
 	-- UI Stuff
 	{
 		src = "https://github.com/lewis6991/gitsigns.nvim",
-		-- version = vim.version.range("1.0.2"),
+		version = vim.version.range("2.1.0"),
 	},
 	{
 		src = "https://github.com/lukas-reineke/indent-blankline.nvim",
-		-- version = vim.version.range("3.9.0"),
+		version = vim.version.range("3.9.1"),
 	},
 	{
 		src = "https://github.com/shortcuts/no-neck-pain.nvim",
-		-- version = vim.version.range("2.5.0"),
+		version = vim.version.range("2.5.3"),
 	},
 	{
 		src = "https://github.com/stevearc/oil.nvim",
-		-- version = vim.version.range("2.15.0"),
+		version = vim.version.range("2.16.0"),
 	},
 
 	{
 		src = "https://github.com/nvim-mini/mini.nvim",
-		-- version = vim.version.range("0.16.0"),
+		version = vim.version.range("0.17.0"),
 	},
 
 	-- ===========================================================================
 	-- LSP / Formatting
 	-- ===========================================================================
-	{ src = "https://github.com/j-hui/fidget.nvim" },
 	{
 		src = "https://github.com/stevearc/conform.nvim",
-		-- version = vim.version.range("9.1.0"),
+		version = vim.version.range("9.1.0"),
 	},
 
 	-- ===========================================================================
@@ -76,35 +75,27 @@ vim.pack.add({
 	-- ===========================================================================
 	{
 		src = "https://github.com/Saghen/blink.cmp",
-		-- version = vim.version.range("1.7.0"),
+		version = vim.version.range("1.10.2"),
 	},
-	{ src = "https://github.com/rafamadriz/friendly-snippets" },
-
-	-- lazydev exposes an ice lua source for neovim configurations
-	-- { src = "https://github.com/folke/lazydev.nvim", version = vim.version.range("1.9.0") },
-
-	-- neocodeium is like copilot or supermaven
 	{
 		src = "https://github.com/supermaven-inc/supermaven-nvim.git",
 		version = "main",
-	},
-	{
-		src = "https://github.com/monkoose/neocodeium",
-		version = vim.version.range("1.16.3"),
 	},
 
 	-- ===========================================================================
 	-- Navigation
 	-- ===========================================================================
-	{ src = "https://github.com/ggandor/leap.nvim.git", version = "main" },
 	{
 		src = "file:////home/macke/dev/home/github.com/ThePrimeagen/harpoon",
 		version = "harpoon2",
 	}, -- Should point to local!
-	{ src = "https://github.com/ibhagwan/fzf-lua", version = "main" },
 
+    -- ===========================================================================
+    -- In Testing
+    -- ===========================================================================
+
+	{ src = "https://github.com/ibhagwan/fzf-lua", version = "main" },
 	{ src = "https://github.com/andymass/vim-matchup" },
-	{ src = "https://github.com/AndrewRadev/tagalong.vim" },
 	{ src = "https://github.com/tpope/vim-sleuth" },
 }, {
 	load = false,
--- lua/aru/plugins/oil.lua
+++ lua/aru/plugins/oil.lua
@@ -2,11 +2,7 @@ local log = require("aru.log")
 
 local ok, oil = pcall(require, "oil")
 if not ok then
-    log:error(
-        ("Failed to load oil.nvim: %s, oil features will be disabled"):format(
-            oil
-        )
-    )
+    log:error(("Failed to load oil.nvim: %s, oil features will be disabled"):format(oil))
 end
 
 local function toggle_oil()
@@ -27,9 +23,72 @@ local function toggle_cwd_oil()
     end
 end
 
+local function parse_git_output(proc)
+    local result = proc:wait()
+    local ignored = {}
+
+    if result.code ~= 0 then return ignored end
+
+    for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+        ignored[line:gsub("/$", "")] = true
+    end
+
+    return ignored
+end
+
+local function new_git_ignored()
+    return setmetatable({}, {
+        __index = function(self, dir)
+            local ignored = {}
+
+            if vim.fn.executable("git") == 1 then
+                ignored = parse_git_output(vim.system({
+                    "git",
+                    "ls-files",
+                    "--ignored",
+                    "--exclude-standard",
+                    "--others",
+                    "--directory",
+                }, {
+                    cwd = dir,
+                    text = true,
+                }))
+            end
+
+            rawset(self, dir, ignored)
+            return ignored
+        end,
+    })
+end
+
+local git_ignored = new_git_ignored()
+local hide_gitignored = true
+local refresh = require("oil.actions").refresh
+local refresh_callback = refresh.callback
+
+local function toggle_gitignored()
+    hide_gitignored = not hide_gitignored
+    refresh_callback()
+    vim.notify(("Oil: %s gitignored files"):format(hide_gitignored and "hiding" or "showing"))
+end
+
+refresh.callback = function(...)
+    git_ignored = new_git_ignored()
+    refresh_callback(...)
+end
+
 require("oil").setup({
     view_options = {
         show_hidden = true,
+        is_always_hidden = function(name, bufnr)
+            if not hide_gitignored then return false end
+
+            local dir = require("oil").get_current_dir(bufnr)
+
+            if not dir then return false end
+
+            return git_ignored[dir][name] == true
+        end,
     },
     float = {
         padding = 5,
@@ -43,6 +102,7 @@ require("oil").setup({
         ["<C-k>"] = "actions.parent",
         ["<C-j>"] = "actions.select",
         ["<C-p>"] = "actions.preview",
+        ["g."] = toggle_gitignored,
     },
 })
 
